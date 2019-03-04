3ed7d59ba36427a82cbeefcc59cc9bf8ed33d1e3
Nicklas Carlsson <nicklas.carlsson@jeppesen.com>
2019-03-04 16:44:14 +0100

    Added tests for add, renamed Kalkylator class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,4 @@
-public class Kalkylator {
+public class Calculator {
     public static void main(String[] args){
         System.out.println("Hepp");
 
---
+++ src/test/java/TestCalkylator_add.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static java.lang.Double.POSITIVE_INFINITY;
+import static org.junit.Assert.assertEquals;
+
+public class TestCalkylator_add {
+
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void add_3_plus_6_is_9(){
+        assertEquals(9,calculator.add(3,6),0);
+    }
+
+    @Test
+    public void add_0_plus_0_is_0(){
+        assertEquals(0,calculator.add(0,0),0);
+    }
+
+    @Test
+    public void add_5_plus_minus_2_is_3(){
+        assertEquals(3,calculator.add(5,-2),0);
+    }
+
+    @Test
+    public void add_maxValue_plus_0_is_maxValue(){
+        assertEquals(1.7E+308,calculator.add(1.7E+308,0),0);
+    }
+
+    @Test
+    public void add_maxValue_plus_maxValue_is_Infinity(){
+        assertEquals(POSITIVE_INFINITY,calculator.add(1.7976931348623157E308,1.7976931348623157E308),0);
+    }
+
+    @Test
+    public void add_Infinity_plus_0_is_Infinity(){
+        assertEquals(POSITIVE_INFINITY,calculator.add(POSITIVE_INFINITY,0),0);
+    }
+
+}
