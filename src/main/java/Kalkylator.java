f4ad3abd1ecc62ce61d756003f421b5a52a01618
Nicklas Carlsson <ncasv92@hotmail.com>
2019-03-04 06:19:40 +0100

    Adding Testlass and some initial fake test

--- src/main/java/Kalkylator.java
+++ src/main/java/Kalkylator.java
@@ -1,5 +1,11 @@
 public class Kalkylator {
     public static void main(String[] args){
         System.out.println("Hepp");
+
     }
+
+    public double add(double d1, double d2){
+        return d1+d2;
+    }
+
 }
---
+++ src/test/java/TestKalkylator.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestKalkylator {
+
+    @Test
+    public void testtest(){
+        assertEquals(false,true);
+    }
+}
